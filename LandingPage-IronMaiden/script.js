69518b78c830bcdb15c4de3f75c7d4ab82800bed
Ricardo R. Figueiredo <ra202310773@univassouras.edu.br>
2023-09-19 18:42:31 -0300

    Update

--- LandingPage-IronMaiden/script.js
+++ LandingPage-IronMaiden/script.js
@@ -5,8 +5,10 @@ playPauseButton.addEventListener("click", function () {
   if (audio.paused) {
     audio.play();
     playPauseButton.innerHTML = "Pausar";
+    playPauseButton.classList.add("paused");
   } else {
     audio.pause();
     playPauseButton.innerHTML = "Reproduzir";
+    playPauseButton.classList.remove("paused");
   }
 });
